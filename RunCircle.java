1a59204ad0b65b8623dc8e5f4b973e2077332475
Insori <82927307+Insori@users.noreply.github.com>
2022-01-07 10:44:55 +0900

    Circle 실행

--- RunCircle.java
+++ RunCircle.java
@@ -1,31 +1,17 @@
-package Test_class3;
+package Test_this2;
 
 public class RunCircle {
 
+	public static void showPrint(Circle c) {
+		System.out.printf("반지름이 %d인 원의 둘레는 %.2f㎝\n", c.getR(), c.getCircum());
+		System.out.printf("반지름이 %d인 원의 면적은 %.2f㎠\n", c.getR(), c.getArea());
+	}
+	
 	public static void main(String[] args) {
-		
-		Circle c1=new Circle();
-		c1.setR(5);	//반지름 5인 원
-		
-		Circle c2=new Circle();
-		c2.setR(12);	//반지름 12인 원
-		
-		c1.calcCircum();	//둘레
-		c1.clacArea();	//면적
-		
-		c2.calcCircum();
-		c2.clacArea();
-
-		System.out.println("---------------------------------------");
-		System.out.printf("반지름이 %d㎝인 원의 둘레는 %.3f㎝\n",c1.getR(),c1.getCircum());
-		System.out.printf("반지름이 %d㎝인 원의 면적은 %.3f㎠\n",c1.getR(),c1.getArea());
-		System.out.println("---------------------------------------");
-		
-		System.out.println("---------------------------------------");
-		System.out.printf("반지름이 %d㎝인 원의 둘레는 %.3f㎝\n",c2.getR(),c2.getCircum());
-		System.out.printf("반지름이 %d㎝인 원의 면적은 %.3f㎠\n",c2.getR(),c2.getArea());
-		System.out.println("---------------------------------------");
-		
+		Circle c1=new Circle(15);
+		c1.showInfo();
+		Circle c2=new Circle(6);
+		c2.showInfo();
 	}
 
 }
