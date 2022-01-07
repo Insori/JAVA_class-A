5e054f49dc2aaf91e096a1bef05f676ab13c58bc
Insori <82927307+Insori@users.noreply.github.com>
2022-01-07 09:51:35 +0900

    this 활용 클래스 생성

---
+++ ThisTest1.java
@@ -0,0 +1,18 @@
+package Test_this1;
+
+public class ThisTest1 {
+	
+	private int n;
+	
+	public void power(int n) {
+		this.n = n * n;
+		System.out.printf("%dÀÇ Á¦°ö½ÂÀº %dÀÌ´Ù.\n",n, this.n);
+	}	
+	public int power(int x, int y) {
+		n=1;
+		for(int i=1; i<=y; i++) {
+			n*=x;
+		}
+		return n;
+	}
+}
