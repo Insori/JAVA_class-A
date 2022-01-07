eb6fb89ce6733b9674e88714c2e912a8b03a5832
Insori <82927307+Insori@users.noreply.github.com>
2022-01-07 10:44:17 +0900

    Circle의 둘레, 면적 구하는 클래스 생성

--- Circle.java
+++ Circle.java
@@ -1,27 +1,36 @@
-package Test_class3;
+package Test_this2;
 
 public class Circle {
 	private int r;
 	private double circum, area;
 	
-	public int getR() {
-		return r;
-	}
-	public void setR(int r) {
-		this.r = r;
-	}
 	public double getCircum() {
 		return circum;
 	}
+	
 	public double getArea() {
 		return area;
 	}
 	
-	public void calcCircum() {
-		circum=2*Math.PI*r;	//원 둘레
+	public int getR() {
+		return r;
 	}
 	
-	public void clacArea() {
-		area=Math.PI*Math.pow(r,2);	//원의 면적		//Math.PI=파이, Math.pow(r,2)=r을 2제곱함
+	public Circle(int r) {
+		this.r=r;
+	}
+	
+	public void calcCircum() {
+		circum=2*Math.PI*r;
+	}
+	
+	public void calcArea() {
+		area=Math.PI*Math.pow(r, 2);
+	}
+	
+	public void showInfo() {
+		calcCircum();
+		calcArea();
+		RunCircle.showPrint(this);
 	}
 }
