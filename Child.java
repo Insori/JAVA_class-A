11c177ee6defaa76ef0252748df8b448ee556b23
Insori <82927307+Insori@users.noreply.github.com>
2022-01-10 11:55:32 +0900

    Protected 상속받는 Child(자식) 클래스 생성

--- Child.java
+++ Child.java
@@ -1,12 +1,17 @@
-package Test_final1;
+package p2;
 
-//부모클래스가 final이면 상속이 금지된다.
+import p1.Parent;
 
 public class Child extends Parent {
-//	@Override
-//	public void rest() {
-//		System.out.println("1시간 휴식한다.");
-//			
-//	}
+	//상속관계일때만 다른 패키지에 있는 protected인  부모의 멤버를 접근할 수 있다.
+	public void testProtected() {
+		num1=100;
+		System.out.println("Parent의 protected field num1: "+num1);
+		
+	}
+	
+	public static void main(String[] args) {
+		Child c=new Child();
+		c.testProtected();
+	}
 }
-
