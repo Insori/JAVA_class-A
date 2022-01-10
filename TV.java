3c7e22412b3100685b003515d73000dcacaeeedd
Insori <82927307+Insori@users.noreply.github.com>
2022-01-10 10:58:59 +0900

    TV 클래스 생성

--- TV.java
+++ TV.java
@@ -1,35 +1,21 @@
-package Test_this3;
+package Test_field1;
 
 public class TV {
+	String color;
+	int size;
+	public static String PRODUCER="LG¿¸¿⁄";
 	
-	private String producer, color;
-	private int size;
-	
-	public String getProducer() {
-		return producer;
-	}
 	public String getColor() {
 		return color;
 	}
 	public int getSize() {
 		return size;
 	}
-	
-	public TV(int size) {
-		this.size=size;
-		System.out.printf("TV(%d) »£√‚µ .\n",this.size);
+	public TV(String color, int size) {
+		this.color = color;
+		this.size = size;
 	}
 	
-	public TV(int size, String color) {
-		this.size=size;
-		this.color=color;
-		System.out.printf("TV(%d, %s) »£√‚µ .\n",this.size, this.color);
-	}
 	
-	public TV(int size, String color, String producer) {
-		this.size=size;
-		this.color=color;
-		this.producer=producer;
-		System.out.printf("TV(%d, %s, %s) »£√‚µ .\n",this.size, this.color, this.producer);
-	}
+	
 }
