3f09fe85ee10e2f6282d5117364992d40f22567d
Insori <82927307+Insori@users.noreply.github.com>
2022-01-10 10:59:16 +0900

    TV 실행

--- RunTV.java
+++ RunTV.java
@@ -1,10 +1,23 @@
-package Test_this3;
+package Test_field1;
 
 public class RunTV {
 
 	public static void main(String[] args) {
-		TV tv1=new TV(55,"Black", "LG전자");
-
+		TV t1=new TV("Black",55);
+		TV t2=new TV("Silver",65);
+		TV t3=new TV("Blue",100);
+		
+//		t1.PRODUCER="삼성전자";	//PRODUCER(static에 할당)은 한 번만 할당되기 때문에 t1,t2,t3 모두 삼성전자라고 나온다.
+		System.out.printf("색상: %s, 크기: %d, 제조사: %s\n",t1.color, t1.size, TV.PRODUCER);
+		System.out.printf("색상: %s, 크기: %d, 제조사: %s\n",t2.color, t2.size, TV.PRODUCER);
+		System.out.printf("색상: %s, 크기: %d, 제조사: %s\n",t3.color, t3.size, TV.PRODUCER);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
