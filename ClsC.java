eb2f10da4cb4d0b377f619d1c549e010e647cacf
Insori <82927307+Insori@users.noreply.github.com>
2022-01-11 10:05:32 +0900

    ClsB를 상속받는 ClsC 클래스 및 생성자 생성

---
+++ ClsC.java
@@ -0,0 +1,11 @@
+package Test_constructorcall2;
+
+public class ClsC extends ClsB{
+	public ClsC() {
+		System.out.println("ClsC()의 생성자 호출");
+	}
+	
+	public ClsC(int n) {
+		System.out.println("ClsC(int n)의 생성자 호출");
+	}
+}
