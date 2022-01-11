b142be8ddaf6035bd986e98a4131fee9342a8d42
Insori <82927307+Insori@users.noreply.github.com>
2022-01-11 10:43:14 +0900

    폭탄, 레이저, 무기 실행

---
+++ RunWeapon.java
@@ -0,0 +1,13 @@
+package Test_overriding1;
+
+public class RunWeapon {
+
+	public static void main(String[] args) {
+		Bomb b=new Bomb();
+		b.fire();
+		Laser l=new Laser();
+		l.fire();
+
+	}
+
+}
