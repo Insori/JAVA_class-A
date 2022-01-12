4becfb5433a13c23d501c8bdce78c1ab758e1ae6
Insori <82927307+Insori@users.noreply.github.com>
2022-01-12 09:16:47 +0900

    Weapon3를 상속받는 군인 클래스 생성

---
+++ Soldier.java
@@ -0,0 +1,15 @@
+package Test_abstract1;
+
+public class Soldier extends Weapon3 {
+
+	@Override
+	public void fire() {
+		System.out.println("ÅÁÅÁÅÁ");
+
+	}
+	
+	public void walk() {
+		System.out.println("±ºÀÎÀÌ °È´Â´Ù.");
+	}
+
+}
