14d0ae2097c418246f8186e7b60e9fa124b32043
Insori <82927307+Insori@users.noreply.github.com>
2022-01-12 11:00:32 +0900

    모바일폰 클래스 생성

---
+++ Mobilephone.java
@@ -0,0 +1,10 @@
+package Test_phone.interface1;
+
+public interface Mobilephone {
+
+		void sendCall(String phoneNumber);
+		void receiveCall(String phoneNumber);
+		void sendSMS(String phoneNumber);
+		void receiveSMS(String phoneNumber);
+		
+}
