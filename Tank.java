fe91e20a7f77af81589d461ffcd3336f474dfa2e
Insori <82927307+Insori@users.noreply.github.com>
2022-01-12 09:17:08 +0900

    Weapon3를 상속받는 탱크 클래스 생성

---
+++ Tank.java
@@ -0,0 +1,14 @@
+package Test_abstract1;
+
+public class Tank extends Weapon3 {
+
+	@Override
+	public void fire() {
+		System.out.println("½´¿ì¿ì¿õ~ ÄçÄç");
+
+	}
+
+	public void forward() {
+		System.out.println("ÅÊÅ©°¡ ÀüÁøÇÑ´Ù.");
+	}
+}
