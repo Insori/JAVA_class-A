9606d6d6493c64b02eef20f9eccd963a413875be
Insori <82927307+Insori@users.noreply.github.com>
2022-01-12 09:17:30 +0900

    Weapon3를 상속받는 전투기 클래스 생성

---
+++ Fighter.java
@@ -0,0 +1,15 @@
+package Test_abstract1;
+
+public class Fighter extends Weapon3 {
+
+	@Override
+	public void fire() {
+		System.out.println("½´¿ì¿õ ÄçÄçÄç");
+
+	}
+	
+	public void fly() {
+		System.out.println("ÀüÅõ±â°¡ ³­´Ù.");
+	}
+
+}
