4d553afab3cc687e260a8878384cd946ab071206
Insori <82927307+Insori@users.noreply.github.com>
2022-01-12 09:18:39 +0900

    Weapon3 오타 수정

--- Weapon3.java
+++ Weapon3.java
@@ -2,7 +2,7 @@ package Test_abstract1;
 
 public abstract class Weapon3 {
 	public void message() {
-		System.out.println("ㅁ기 클래스입니다.");
+		System.out.println("무기 클래스입니다.");
 	}
 	
 	public abstract void fire();
