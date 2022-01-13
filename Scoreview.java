1c48eb9a743647aaa72c238306e5be554cf383d5
Insori <82927307+Insori@users.noreply.github.com>
2022-01-13 11:52:32 +0900

    성적, 합계, 평균 구하는 클래스 추가

--- Scoreview.java
+++ Scoreview.java
@@ -2,11 +2,11 @@ package mvc.score.view;
 
 import java.util.ArrayList;
 import mvc.score.model.Student;
-
+//MVC Modeling 중에서 View(보여줌)에 해당하는 클래스 : 출력
 public class Scoreview {
 
 	private ArrayList<Student> studentArrList;
-
+	
 	public Scoreview(ArrayList<Student> studentArrList) {
 //		super();
 		this.studentArrList = studentArrList;
@@ -17,8 +17,21 @@ public class Scoreview {
 		
 		for(int i=0; i<studentArrList.size(); i++) {
 			Student s=studentArrList.get(i);
-			System.out.printf("* 성명: %s  학번: %d  국어: %d  영어: %d \n");
+			int sum=calcSum(s);
+			double avg=calcAvg(sum);
+			System.out.printf("* 성명: %s  학번: %d  국어: %d  영어: %d  수학: %d  과학:  %d  역사: %d  합계: %d  평균: %.2f\n",
+					s.getName(),s.getStuId(),s.getKor(),s.getEng(), s.getMath(), s.getScience(),s.getHistory(), sum,avg);
 		}
+	}
+	
+	public int calcSum(Student s) {
+		int sum=s.getKor()+s.getEng()+s.getMath()+ s.getScience()+s.getHistory();
+		return sum;
+	}
+	
+	public double calcAvg(int sum) {
+		double avg=sum/5.0;
+		return avg;
 	}
 	
 }
